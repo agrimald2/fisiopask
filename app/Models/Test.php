d9f943966fad08907a3f7747d0b2910a00b688eb
Marcelo Colonia <mcoloniab18@gmail.com>
2022-02-08 22:35:41 +0100

    add relationships

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -10,5 +10,30 @@ class Test extends Model
     use HasFactory;
 
     protected $table = "general_test";
+    protected $guarded = ["id", "created_at", "updated_at"];
+
+    /**
+     * Relationships
+     */
+
+    public function doctor() 
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function patient() 
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function company() 
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function testType() 
+    {
+        return $this->belongsTo(TestType::class);
+    }
     
-x
+}
