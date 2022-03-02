45889a0675daa3064f6ffa39fa2d7b6645d8775a
Marcelo Colonia <mcoloniab18@gmail.com>
2022-03-02 19:43:18 +0100

    cleanup

--- app/Http/Controllers/Backend/PatientRates/CancelRateAction.php
+++ app/Http/Controllers/Backend/PatientRates/CancelRateAction.php
@@ -3,18 +3,11 @@
 namespace App\Http\Controllers\Backend\PatientRates;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 use App\Models\PatientRate;
 
 class CancelRateAction extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke(PatientRate $rate)
     {
         $rate->state = PatientRate::RATE_STATUS_CANCELED;
