1d7e7f4c18e6882a64ebe6fdc12ce7849447fbd2
agrimal2 <agrimaldopci18@gmail.com>
2023-09-14 02:19:00 -0500

    Add Payment to assitant

--- resources/js/Pages/Backend/Index/Assistants/Links.vue
+++ resources/js/Pages/Backend/Index/Assistants/Links.vue
@@ -1,17 +1,11 @@
 <template>
   <!-- Usuarios y Personas -->
   <link-group title="Usuarios y Personas">
-    <link-item
-      label="Gráficos y Estadística"
-      icon="fa-chart-bar"
-      @click="go('statistics.index')"
-    />
+    <link-item label="Gráficos y Estadística" icon="fa-chart-bar" @click="go('statistics.index')" />
 
-    <link-item
-      label="Citas"
-      icon="fa-eye"
-      @click="go('doctors.appointments.index')"
-    />
+    <link-item label="Citas" icon="fa-eye" @click="go('doctors.appointments.index')" />
+
+    <link-item label="Pagos recibidos" icon="fa-receipt" @click="go('paymentsPayments.index')" />
   </link-group>
 </template>
 
