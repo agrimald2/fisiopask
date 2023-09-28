e66a6563d03665af6f333982b40ea77b4c751665
agrimal2 <agrimaldopci18@gmail.com>
2023-09-28 15:35:25 -0500

    .

--- resources/views/landing/head.blade.php
+++ resources/views/landing/head.blade.php
@@ -1,3 +1,41 @@
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LWXMWBFVT4"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LWXMWBFVT4');
+    </script>
+    <!-- End Google tag (gtag.js) -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11334963934"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-11334963934');
+    </script>
+    <!-- End Google tag (gtag.js) -->
+
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1694773377446249');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1694773377446249&ev=PageView&noscript=1"/></noscript>
+    <!-- End Meta Pixel Code -->
+
     <!-- Meta Tags -->
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -7,19 +45,19 @@
     <meta property="og:image" content="{{ env('APP_URL') }}/logo-white-large.png">
     <link rel="icon" href="{{ env('APP_URL') }}/logo-white-large.png) }}">
     
-<!-- META TAGS -->
-<!-- Primary Meta Tags -->
+    <!-- META TAGS -->
+    <!-- Primary Meta Tags -->
     <title>Fisiosalud - Centro de rehabilitación y fisioterapia</title>
     <meta name="title" content="Fisiosalud">
     <meta name="description" content="Centro de rehabilitación especializado en terapia traumatológica , fisioterapia deportiva, y fisioterapia estética. Incorporamos técnicas de recuperación rápida, liderado por licenciados profesionales con experiencia internacional">
 
-<!-- Open Graph / Facebook -->
+    <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://fisiosalud.pe/">
     <meta property="og:title" content="Fisiosalud">
     <meta property="og:description" content="Centro de rehabilitación especializado en terapia traumatológica , fisioterapia deportiva, y fisioterapia estética. Incorporamos técnicas de recuperación rápida, liderado por licenciados profesionales con experiencia internacional">
 
-<!-- Twitter -->
+    <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://fisiosalud.pe/">
     <meta property="twitter:title" content="Fisiosalud">
@@ -54,37 +92,3 @@
     <script src="landing/js/textRotate.min.js"></script>
     <script src="landing/js/select2.min.js"></script>
     <script src="landing/js/main.js"></script>
-    
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TF6PTM7');</script>
-    <!-- End Google Tag Manager -->
-
-
-    <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1694773377446249');
-    fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1694773377446249&ev=PageView&noscript=1"
-    /></noscript>
-
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11334963934"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'AW-11334963934');
-    </script>
